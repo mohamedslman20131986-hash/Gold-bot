8d192eba921452be4951aac4bb310c463e0a461e
mohamedslman20131986-hash <mohamed.slman2013.1986@gmail.com>
2026-04-06 11:11:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 
+last_price = None
+
 def get_gold_price():
     try:
         url = "https://api.gold-api.com/price/XAU"
         response = requests.get(url)
         data = response.json()
-        price = data["price"]
-        return price
+        ounce_price = data["price"]
+
+        # تحويل الى غرام
+        gram_price_usd = ounce_price / 31.1035
+
+        return gram_price_usd
     except:
         return None
 
@@ -27,9 +33,35 @@ while True:
     price = get_gold_price()
     
     if price:
-        message = f"💰 سعر الذهب الآن:\n{price} دولار للأونصة"
-    else:
-        message = "❌ فشل في جلب السعر"
+        global last_price
+        
+        # تحويل للعراقي (تقريباً 1300 دينار لكل دولار)
+        price_iq = price * 1300
 
-    send_telegram_message(message)
+        # تحديد الاتجاه
+        if last_price is None:
+            trend = "➖ أول تحديث"
+        elif price > last_price:
+            trend = "🔺 صاعد"
+        elif price < last_price:
+            trend = "🔻 نازل"
+        else:
+            trend = "⏸️ ثابت"
+
+        # منع التكرار
+        if price != last_price:
+            message = f"""💰 سعر الذهب الآن:
+
+🔸 {price:.2f} دولار / غرام
+🔸 {int(price_iq):,} دينار عراقي
+
+📊 الحالة: {trend}
+"""
+            send_telegram_message(message)
+
+            last_price = price
+
+    else:
+        send_telegram_message("❌ فشل في جلب السعر")
+
     time.sleep(60)
